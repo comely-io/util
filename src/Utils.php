d04d68f92fbff239dfe265a9e986b2f5b6261f93
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-06-20 13:42:28 +0500

    version

--- src/Utils.php
+++ src/Utils.php
@@ -21,7 +21,7 @@ namespace Comely\Utils;
 class Utils
 {
     /** string Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "1.0.10";
+    public const VERSION = "1.1.10";
     /** int Version (Major * 10000 + Minor * 100 + Release) */
-    public const VERSION_ID = 10010;
+    public const VERSION_ID = 10110;
 }
