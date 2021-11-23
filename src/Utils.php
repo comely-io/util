7de089a1f6cf5c525c1794294f98bc46220839be
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-11-23 22:33:06 +0500

    prep 2.0.4

--- src/Utils.php
+++ src/Utils.php
@@ -21,7 +21,7 @@ namespace Comely\Utils;
 class Utils
 {
     /** string Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "2.0.3";
+    public const VERSION = "2.0.4";
     /** int Version (Major * 10000 + Minor * 100 + Release) */
-    public const VERSION_ID = 20003;
+    public const VERSION_ID = 20004;
 }
